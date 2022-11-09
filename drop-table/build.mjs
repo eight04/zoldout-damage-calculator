78d4ed7dc410e6d37cf3e1a3978e81a5bd56cdae
eight04 <eight04@gmail.com>
2022-11-10 06:27:40 +0800

    ap reduc is over

--- drop-table/build.mjs
+++ drop-table/build.mjs
@@ -32,7 +32,7 @@ const sortedItems = [...items.entries()]
   .sort((a, b) => getDifficulty(a.list[0].stage.name) - getDifficulty(b.list[0].stage.name));
 
 await writeFile(new URL("README.md", import.meta.url), `
-# Zold:Out Drop Table (主線[普通]體力減半)
+# Zold:Out Drop Table
 
 ${drawTable(sortedItems)}
 `);
@@ -76,16 +76,10 @@ function aToIndex(s) {
 }
 
 function dropsToLine(drops) {
-  return Object.entries(drops).map(([key, value]) => `${value}${key}`).join("/");
+  return Object.entries(drops).map(([key, value]) => `${value.toFixed(1)}${key}`).join("/");
 }
 
-async function loadStages(doc) {
-  try {
-    const s = await readFile(new URL("stages.json", import.meta.url), "utf8");
-    return JSON.parse(s);
-  } catch (err) {
-    // pass
-  }
+async function loadStagesFromSilverWind() {
   const sheet = doc.sheetsByTitle["素材"];
   await sheet.loadCells("A4:C");
 
@@ -97,7 +91,6 @@ async function loadStages(doc) {
       drops: {}
     };
     const name = sheet.getCell(i, aToIndex("A"));
-    console.log(name.value);
     stage.name = name.value;
 
     for (const s of ["B", "C"].map(key => sheet.getCell(i, aToIndex(key)).value)) {
@@ -108,17 +101,59 @@ async function loadStages(doc) {
           throw new Error(`duplicate item ${stage.name}, ${name}`);
         }
         stage.drops[name] = n ? Number(n) : 1;
-
-        if (/普通/.test(stage.name)) {
-          stage.drops[name] *= 2;
-        }
       }
     }
 
     stages.push(stage);
   }
-  await writeFile(new URL("stages.json", import.meta.url), JSON.stringify(stages, null, 2));
 
+  return stages;
+}
+
+async function loadStagesFromEight(stages) {
+  const doc = new gs.GoogleSpreadsheet("12CqUiGojYOfVL-CZBvMgq4EAGumMHsbmYmgagBWAdMk");
+  doc.useApiKey("AIzaSyBmF9PBdznx-Dpxa2YOWWK6gcThwPFpLDM");
+  await doc.loadInfo();
+  for (const stage of stages) {
+    let sheet;
+    const [, lab, sup] = /^(\D+)(.+)/.exec(stage.name);
+    const keys = [sup + lab, lab === "普通" ? sup : undefined].filter(Boolean);
+    for (const key of keys) {
+      if (doc.sheetsByTitle[key]) {
+        sheet = doc.sheetsByTitle[key];
+        break;
+      }
+    }
+    if (!sheet) continue;
+
+    console.log(stage.name);
+    await sheet.loadCells("A1:2");
+    const map = new Map;
+    for (let i = 0; i < sheet.columnCount; i++) {
+      map.set(sheet.getCell(0, i).value, sheet.getCell(1, i).value);
+    }
+    const rMap = new Map([...map.entries()].map(([l, r]) => [r, l]));
+    console.log(rMap);
+    const ap = map.get("體力");
+
+    for (const key in stage.drops) {
+      if (rMap.has(key)) {
+        stage.drops[key] = rMap.get(key) * ap;
+      }
+    }
+  }
+}
+
+async function loadStages() {
+  try {
+    const s = await readFile(new URL("stages.json", import.meta.url), "utf8");
+    return JSON.parse(s);
+  } catch (err) {
+    // pass
+  }
+  const stages = await loadStagesFromSilverWind();
+  await loadStagesFromEight(stages);
+  await writeFile(new URL("stages.json", import.meta.url), JSON.stringify(stages, null, 2));
   return stages;
 }
 
