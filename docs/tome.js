4e9e66d0d504355a9735c64a0b17b8785c94ef39
eight04 <eight04@gmail.com>
2023-03-06 15:31:40 +0800

    Build

--- docs/tome.js
+++ docs/tome.js
@@ -1,2 +1,2 @@
-var tome=function(){"use strict";return new tome0D776430.A({target:document.body,props:{weapons:tome0D776430.w,storeKey:"tome"}})}();
+var tome=function(){"use strict";return new tome5E2E931D.A({target:document.body,props:{weapons:tome5E2E931D.w,storeKey:"tome"}})}();
 //# sourceMappingURL=tome.js.map
